8aa36bfd5aabbb0a25d9b7e876d6e87041d1351d
Servajean Maximilien <maximiliense@gmail.com>
2020-02-04 15:34:13 +0100

    patchs

--- examples/plot.py
+++ examples/plot.py
@@ -2,11 +2,11 @@ from environmental_raster_glc import PatchExtractor
 
 extractor = PatchExtractor('/home/data/rasters_GLC20/archive/soilgrids/', size=256, verbose=True)
 
-extractor.append('bdticm')
+# extractor.append('bdticm')
 # extractor.append('text')
 # extractor.append('clc')
 # extractor.append('bs_top')
 # extractor.append('oc_top')
 
-# extractor.add_all()
+extractor.add_all()
 extractor.plot((37.746517, -122.423786))
