5d97b2e1c5bdca65874001b6ee3ba7629109eee1
LORIEUL Titouan <titouan.lorieul@inria.fr>
2021-03-05 20:45:16 +0100

    Added plotting utility for patches

---
+++ plotting.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+
+
+def visualize_observation_patch(patch, landcover_labels=None):
+    rgb_patch, near_ir_patch, altitude_patch, landcover_patch = patch
+
+    if landcover_labels is None:
+        n_labels = np.max(landcover_patch)+1
+        landcover_labels = np.arange(n_labels)
+
+    cmap = plt.cm.get_cmap("viridis", len(landcover_labels))
+
+    legend_elements = []
+    for landcover_label, color in zip(landcover_labels, cmap.colors):
+        legend_elements.append(
+            Patch(color=color, label=landcover_label)
+        )
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(9, 9))
+    axes = axes.ravel()
+    axes_iter = iter(axes)
+
+    ax = next(axes_iter)
+    ax.imshow(rgb_patch)
+    ax.set_title("RGB satellite image")
+
+    ax = next(axes_iter)
+    ax.imshow(near_ir_patch, cmap="gray")
+    ax.set_title("Near-IR satellite image")
+
+    ax = next(axes_iter)
+    im = ax.imshow(altitude_patch)
+    ax.set_title("Altitude")
+    fig.colorbar(im, ax=ax)
+
+    ax = next(axes_iter)
+    im = ax.imshow(landcover_patch, interpolation="none", cmap=cmap, vmin=0, vmax=len(legend_elements))
+    ax.set_title("Land cover")
+    visible_landcover_categories = np.unique(landcover_patch)
+    legend = [legend_elements[i] for i in visible_landcover_categories]
+    ax.legend(handles=legend, handlelength=.75, bbox_to_anchor=(1, 0.5), loc="center left")
+
+    for ax in axes:
+        ax.axis("off")
+
+    fig.tight_layout()
