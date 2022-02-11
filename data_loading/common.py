a56c2eee5649cf636107a16b6b81705a37786e47
LORIEUL Titouan <titouan.lorieul@inria.fr>
2022-02-11 16:34:48 +0100

    Added possibility to only load some types of patches in common.py using data parameter

--- data_loading/common.py
+++ data_loading/common.py
@@ -5,7 +5,7 @@ from PIL import Image
 import tifffile
 
 
-def load_patch(observation_id, patches_path, landcover_mapping=None, return_arrays=True):
+def load_patch(observation_id, patches_path, *, data="all", landcover_mapping=None, return_arrays=True):
     """Loads the patch data associated to an observation id
 
     Parameters
@@ -14,6 +14,8 @@ def load_patch(observation_id, patches_path, landcover_mapping=None, return_arra
         Identifier of the observation.
     patches_path : string / pathlib.Path
         Path to the folder containing all the patches.
+    data : string or list of string
+        Specifies what data to load, possible values: 'all', 'rgb', 'near_ir', 'landcover' or 'altitude'.
     landcover_mapping : 1d array-like
         Facultative mapping of landcover codes, useful to align France and US codes.
     return_arrays : boolean
@@ -39,22 +41,35 @@ def load_patch(observation_id, patches_path, landcover_mapping=None, return_arra
 
     filename = Path(patches_path) / region / subfolder1 / subfolder2 / observation_id
 
-    rgb_filename = filename.with_name(filename.stem + "_rgb.jpg")
-    rgb_patch = Image.open(rgb_filename)
+    patches = []
 
-    near_ir_filename = filename.with_name(filename.stem + "_near_ir.jpg")
-    near_ir_patch = Image.open(near_ir_filename)
+    if data == "all":
+        data = ["rgb", "near_ir", "landcover", "altitude"]
 
-    landcover_filename = filename.with_name(filename.stem + "_landcover.tif")
-    landcover_patch = tifffile.imread(landcover_filename)
-    if landcover_mapping is not None:
-        landcover_patch = landcover_mapping[landcover_patch]
+    if "rgb" in data:
+        rgb_filename = filename.with_name(filename.stem + "_rgb.jpg")
+        rgb_patch = Image.open(rgb_filename)
+        if return_arrays:
+            rgb_patch = np.asarray(rgb_patch)
+        patches.append(rgb_patch)
 
-    altitude_filename = filename.with_name(filename.stem + "_altitude.tif")
-    altitude_patch = tifffile.imread(altitude_filename)
+    if "near_ir" in data:
+        near_ir_filename = filename.with_name(filename.stem + "_near_ir.jpg")
+        near_ir_patch = Image.open(near_ir_filename)
+        if return_arrays:
+            near_ir_patch = np.asarray(near_ir_patch)
+        patches.append(near_ir_patch)
 
-    if return_arrays:
-        rgb_patch = np.asarray(rgb_patch)
-        near_ir_patch = np.asarray(near_ir_patch)
+    if "altitude" in data:
+        altitude_filename = filename.with_name(filename.stem + "_altitude.tif")
+        altitude_patch = tifffile.imread(altitude_filename)
+        patches.append(altitude_patch)
 
-    return (rgb_patch, near_ir_patch, altitude_patch, landcover_patch)
+    if "landcover" in data:
+        landcover_filename = filename.with_name(filename.stem + "_landcover.tif")
+        landcover_patch = tifffile.imread(landcover_filename)
+        if landcover_mapping is not None:
+            landcover_patch = landcover_mapping[landcover_patch]
+        patches.append(landcover_patch)
+
+    return patches
