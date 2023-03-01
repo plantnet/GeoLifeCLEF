2a5fae5db68235491cbc6877241510ffeeb3948d
bdeneu <Reilonia#73!>
2023-03-01 14:47:27 +0100

    add useful_codes

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This repository is related to the GeoLifeCLEF challenge. The details of the challenge, the data, and all other useful information are present on the challenge page: _release soon !_
 
-In this repository you will find two main folders ``data_preparation`` and ``sample_codes``.
+In this repository you will find two main folders ``data_preparation`` and ``useful_codes``.
 - In ``data_preparation`` you will find the main codes used during the preparation of the dataset.
-- In ``sample_codes`` we provide useful codes and examples to help the challenge participants to handle the data and produce runs.
+- In ``useful_codes`` we provide useful codes and examples to help the challenge participants to handle the data and produce runs.
---
+++ useful_codes/data/datasets.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+
+class PatchesDataset(Dataset):
+    def __init__(
+        self,
+        occurrences,
+        providers,
+        transform=None,
+        target_transform=None,
+        id_name="glcID",
+        label_name="speciesId",
+        item_columns=('lat', 'lon', 'patchID'),
+    ):
+        self.occurences = Path(occurrences)
+        self.providers = providers
+        self.transform = transform
+        self.target_transform = target_transform
+
+        df = pd.read_csv(self.occurences, sep=";", header='infer', low_memory=False)
+
+        self.observation_ids = df[id_name].values
+        self.items = df[item_columns]
+        self.targets = df[label_name].values
+
+    def __len__(self):
+        return len(self.observation_ids)
+
+    def __getitem__(self, index):
+        item = self.items.iloc[index].to_dict()
+
+        patches = []
+        for provider in self.providers:
+            patches.append(provider[item])
+
+        # Concatenate all patches into a single tensor
+        if len(patches) == 1:
+            patches = patches[0]
+        else:
+            patches = np.concatenate(patches, axis=0)
+
+        if self.transform:
+            patches = self.transform(patches)
+
+        target = self.targets[index]
+
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        return torch.from_numpy(patches).float(), target
